2987d3a763f996b3d2e33b9200b1da3b03f8f67d
Antonskola07 <179210895+Antonskola07@users.noreply.github.com>
2024-11-22 13:47:39 +0100

    n

---
+++ Game1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SharpDX.XInput;
+
+namespace game;
+
+public class Game1 : Game
+{
+
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    Texture2D pixel;
+
+    Rectangle paddleLeft=new Rectangle(x:10,y:200,width:20,height:100);
+
+    Rectangle paddeleRight= new Rectangle(x:770,y:200,width:20,100);
+
+    Rectangle ball= new Rectangle(390,y:230,width:20,height:20);
+    
+    if(GamePad.Getstate(playerIndex: PlayerIndex.one) .Buttons.Back == Buttonstate.pressed ||)
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
